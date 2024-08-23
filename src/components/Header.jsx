3280c0e42c6f26d6cf0dbeb5b0daac546eb5ec60
Vkushwahaa <kushwahavinay62@gmail.com>
2024-08-24 02:51:07 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-const Header = ({tasks}) => {
+const Header = ({ tasks }) => {
   return (
     <header>
       <h1 className="heading text-center">To-Do-List</h1>
       {tasks.length === 0 ? (
         <h2 className="text-center">you have completated all your tasks</h2>
       ) : tasks.length === 1 ? (
-        <h2>you have task {tasks.length} left</h2>
+        <h2 className="text-center">you have task {tasks.length} left</h2>
       ) : (
-        <h2>you have tasks {tasks.length} left</h2>
+        <h2 className="text-center">you have tasks {tasks.length} left</h2>
       )}
     </header>
   );
