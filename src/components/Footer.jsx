adf003479c1547051cd3277361692fdb1c0e7ead
Vkushwahaa <kushwahavinay62@gmail.com>
2024-08-24 02:49:53 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
-const Footer = (tasks) => {
-    const today = new Date()
+const Footer = () => {
+  const today = new Date();
+  const formattedDate = `${today.getDate().toString().padStart(2, "0")}/${(
+    today.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, "0")}/${today.getFullYear()}`;
+
   return (
-    <footer className="mt-5">
-      <h2 className="footer text-center">{today.toLocaleString()}</h2>
+    <footer className=" footer mt-5">
+      <h2 className="name">@vkushwahaa</h2>
+      <h2 className="date-footer">{formattedDate}</h2>
     </footer>
   );
 };
